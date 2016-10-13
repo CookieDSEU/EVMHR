75799d8423d4efe39f9a530295c838eac35f6a4e
Tsehsuan Wang <holywrong@gmail.com>
2016-10-13 15:12:25 +0800

    [Change] OpenCV.mk relative directory

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -10,7 +10,7 @@ OPENCV_LIB_TYPE := STATIC
 
 #设定OpenCV.mk文件的地址
 ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
-include E:\JAVA_soft\EVMHR\native\jni\OpenCV.mk  #注意这里要根据自己的native地址修改
+include $(LOCAL_PATH)\native\jni\OpenCV.mk
 else
 include $(OPENCV_MK_PATH)
 endif
