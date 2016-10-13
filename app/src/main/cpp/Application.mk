57d44ac1ce89d9fc00fb4ec1f29239bff024c2ad
Tsehsuan Wang <holywrong@gmail.com>
2016-10-13 15:10:26 +0800

    Update Application.mk

--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,4 +1,4 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions -std=c++11 -O3 #C++编译器flag
-APP_ABI := srmeabi-v7a #可以自己设定编译平台，如armeabi，all是所有平台
+APP_ABI := armeabi-v7a #可以自己设定编译平台，如armeabi，all是所有平台
 APP_OPTIM := release #设定编译动态库的版本 debug或者release
